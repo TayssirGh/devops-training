3121a7a1f1512f18ac7e65ee15fffbae3315d7b5
tmalek <tmalek.touat@gmail.com>
2021-08-06 11:50:24 +0100

    updates2

--- results/ex-2-s-2/build-docker-images.sh
+++ results/ex-2-s-2/build-docker-images.sh
@@ -1,7 +1,10 @@
-Build images :
+docker run --rm -p 82:80 --mount type=bind,source="$(pwd)/src/assets/configs/app-config.json",target="/usr/share/nginx/html/assets/configs/app-config.json" website
 
-docker build -t backend -f Dockerfile .
+docker run --rm -p 8420:4000 --env-file .env qr
 
-docker run --rm -p 3200:5000 --env-file .env qr
+docker run --rm -p 81:3000 --env-file .env backend
 
+docker build -t orcasifilus/t-003-qr -f Dockerfile .
+
+docker push orcasifilus/t-003-qr
 
