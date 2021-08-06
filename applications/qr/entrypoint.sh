281eed0139ede02e80643e54994dbf8249336115
mghodben <marwen.ghodben@comwork.io>
2021-08-06 15:09:43 +0100

    change entrypoint.sh and results.sh

--- applications/qr/entrypoint.sh
+++ applications/qr/entrypoint.sh
@@ -3,4 +3,4 @@ if [[ -z "$APP_PORT" ]]; then
   echo "The 'App_PORT' variable is required!"
   exit 1
 fi
-uvicorn app:atom --host 0.0.0.0 --port "$APP_PORT"
+uvicorn app:atom --reload --host 0.0.0.0 --port "$APP_PORT"
--- results/ex-1-s2/results.sh
+++ results/ex-1-s2/results.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
-if [[ -z "$APP_PORT" ]]; then
-  echo "The 'App_PORT' variable is required!"
-  exit 1
-else
-  echo "The 'App_PORT' variable is empty"
-fi
-if [[ -n "$MONGO_URI" ]]; then
-  echo "The 'MONGO_URI' variable is required!"
-  exit 1
-else
-  echo "The 'MONGO_URI' variable is empty"
-fi
-if [[ -n "$MONGO_DB" ]]; then
-  echo "The 'MONGO_DB' variable is required!"
-  exit 1
-else
-  echo "The 'MONGO_DB' variable is empty"
-fi
-
-docker build -t mghodben/t001-qr .
-docker build -t mghodben/t001-backend .
-docker build -t mghodben/t001-website .
-docker run --rm -p 8420:8420 --env-file qr.env mghodben/t001-qr
-docker run --rm -p 3000:3000 --env-file backend.env mghodben/t001-backend
-docker run --rm -p 4200:80 --mount type=bind,source="$(pwd)/app-config.json",target="/app/assets/configs/app-config.json" mghodben/t001-website
+docker build -t  mghodben/t001-qr -f ../../applications/qr/Dockerfile ../../applications/qr/.
+docker build -t  mghodben/t001-backend -f ../../applications/backend/Dockerfile ../../applications/backend/.
+docker build -t mghodben/t001-website -f  ../../applications/website/Dockerfile .../../applications/website/.
+docker run -d -p 8420:8420  mghodben/t001-qr
+docker run -d -p 3000:3000 --env-file ./backend.env mghodben/t001-backend
+docker run -d -p 4200:80 --mount type=bind,source="/home/marwen/Documents/devops-training/results/ex-1-s2/app-config.json",target="/website/app/assets/configs/app-config.json" mghodben/t001-website
