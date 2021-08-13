108d4c7dc01b8bf8812978c1cd88d0423eb4bc72
tmalek <tmalek.touat@gmail.com>
2021-08-13 13:09:06 +0100

    ex2-3 changes 2.0

--- ci-scripts/gitrunner.sh
+++
@@ -1,8 +0,0 @@
-
-sudo gitlab-runner register \
-  --url "https://gitlab.comwork.io/comwork_training/devops-training/" \
-  --registration-token "26b6d80f-61af-4770-9482-d2156345dbbb" \
-  --description "docker-ruby:2.6" \
-  --executor "docker" \
-  --template-config /applicaitons/app-test-config.template.toml \
-  --docker-image ruby:2.6
