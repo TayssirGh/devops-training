ec8e36c41281f8966348eb59683419fb2ab405f7
tmalek <tmalek.touat@gmail.com>
2021-08-13 15:04:39 +0100

    some other changes

--- ci-scripts/image-builder.sh
+++ ci-scripts/image-builder.sh
@@ -3,15 +3,15 @@
 build() {
   tag=${1}
 
-  echo "Building ${T003_USERNAME}/t003-${IMAGE_NAME}:${tag}"
-  docker build -t "${T003_USERNAME}/t003-${IMAGE_NAME}:${tag}" \
+  echo "Building ${T-003_USERNAME}/t-003-${IMAGE_NAME}:${tag}"
+  docker build -t "${T-003_USERNAME}/t-003-${IMAGE_NAME}:${tag}" \
     -f "./applications/${IMAGE_NAME}/Dockerfile" "./applications/${IMAGE_NAME}"
 
-  echo "Pushing ${T003_USERNAME}/t003-${IMAGE_NAME}:${tag}"
-  docker push "${T003_USERNAME}/t003-${IMAGE_NAME}:${tag}"
+  echo "Pushing ${T-003_USERNAME}/t-003-${IMAGE_NAME}:${tag}"
+  docker push "${T-003_USERNAME}/t-003-${IMAGE_NAME}:${tag}"
 }
 
 for tag in ${IMAGE_TAG}; do
   echo "Building tag : ${tag}"
-  build "${tag}"
+  build "${tag}" || exit 1
 done
