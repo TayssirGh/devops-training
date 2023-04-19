b281cf871b6d094e9ba703b884a5a7905d20bb47
fglaa <glaafarah@gmail>
2023-04-19 12:12:10 +0100

    Making apps work

--- applications/backend/clients/qr-client.js
+++ applications/backend/clients/qr-client.js
@@ -1,12 +1,17 @@
 const axios = require('axios')
+require('dotenv').config();
+
 const {
     QR_URL
 } = process.env;
 
+const myVariable = process.env.QR_URL;
+console.log(myVariable)
+
 class QrClient {
 
     generateQr(qr_id, qr_content) {
-        return axios.default.post(QR_URL, {
+        return axios.default.post(myVariable, {
             id: qr_id,
             content: qr_content
         })
--- applications/backend/index.js
+++ applications/backend/index.js
@@ -1,15 +1,19 @@
 const api = require('./config/api')
+require('dotenv').config();
+
 const {
     APP_PORT
 } = process.env
+
+const myVariable = process.env.APP_PORT;
 
 const databaseConnection = require('./config/database')
 
 databaseConnection()
     .then(() => {
         console.info('Successfully connected to database.')
-        api.listen(APP_PORT, () => {
-            console.log(`Listening on port ${APP_PORT}`);
+        api.listen(myVariable, () => {
+            console.log(`Listening on port ${myVariable}`);
         })
     })
     .catch(reason => {
