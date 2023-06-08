c3edd10d76c187c6029ec821b6f35e79eb10fcdb
xqliza <khoubeibmannai@outlook.com>
2023-06-08 18:24:15 +0100

    fixed env variables

--- ci-scripts/build_push_images.sh
+++ ci-scripts/build_push_images.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker build applications/${image_name} -t "${registry_name}/t005-${image_name}:${tag_name}"
+docker build applications/${image_name} -t "${T005_REGISTRY_USER_NAME}/t005-${image_name}:${tag_name}"
 
-docker push "registry_name/t005-${image_name}:${tag_name}"
+docker push "${T005_REGISTRY_USER_NAME}/t005-${image_name}:${tag_name}"
