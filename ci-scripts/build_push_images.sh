e790255aab1f015fa4fb542cef97a825eac9c344
Karim Zomita <Zomitaka@gmail.com>
2023-06-08 21:07:28 +0100

    change init method

--- ci-scripts/build_push_images.sh
+++ ci-scripts/build_push_images.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 init(){
- systemctl start docker
+ service docker start
 }
 build(){
     docker build applications/${image_name} -t "${T006_REGISTRY_USER_NAME}/t006-${image_name}:${image_tag}"
