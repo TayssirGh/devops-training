60edeebba0f05a8a5f35f525fc5cba93e9990c8d
Karim Zomita <Zomitaka@gmail.com>
2023-06-08 20:51:23 +0100

    add init for dockerd

--- ci-scripts/build_push_images.sh
+++ ci-scripts/build_push_images.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
-docker build applications/${image_name} -t "${T006_REGISTRY_USER_NAME}/t006-${image_name}:${image_tag}"
+init(){
+ dockerd
+}
+build(){
+    docker build applications/${image_name} -t "${T006_REGISTRY_USER_NAME}/t006-${image_name}:${image_tag}"
+    docker push "${T006REGISTRY_USER_NAME}/t006-${image_name}:${image_tag}"
+}
 
-docker push "${T006REGISTRY_USER_NAME}/t006-${image_name}:${image_tag}"
+echo "build image ${image_name} ${image_tag}"
+init
+build
