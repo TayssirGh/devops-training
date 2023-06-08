18e7a44648b3c8d1e6a9697fac8db3b3803b50f3
Karim Zomita <Zomitaka@gmail.com>
2023-06-08 22:28:14 +0100

    update script

--- ci-scripts/build_push_images.sh
+++ ci-scripts/build_push_images.sh
@@ -3,7 +3,7 @@
 
 build(){
     docker build applications/${image_name} -t "${T006_REGISTRY_USER_NAME}/t006-${image_name}:${image_tag}"
-    docker push "${T006REGISTRY_USER_NAME}/t006-${image_name}:${image_tag}"
+    docker push "${T006_REGISTRY_USER_NAME}/t006-${image_name}:${image_tag}"
 }
 
 echo "build image ${image_name} ${image_tag}"
