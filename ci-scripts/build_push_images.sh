ba1a9bff4b560ad3c1e22b244f11a30cb16672a1
xqliza <khoubeibmannai@outlook.com>
2023-06-08 20:13:03 +0100

    fix gitlabci

--- ci-scripts/build_push_images.sh
+++ ci-scripts/build_push_images.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker build applications/${image_name} --tag "${T005_REGISTRY_USER_NAME}/t005-${image_name}:${tag_name}"
+docker build applications/${image_name} --tag "${T005_REGISTRY_USER_NAME}/t005-${image_name}:${image_tag}"
 
-docker push "${T005_REGISTRY_USER_NAME}/t005-${image_name}:${tag_name}"
+docker push "${T005_REGISTRY_USER_NAME}/t005-${image_name}:${image_tag}"
