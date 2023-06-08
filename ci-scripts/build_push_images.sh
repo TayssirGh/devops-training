5134181d5af7b93842cd7c5cb6ebdcb9705b4c82
Karim Zomita <Zomitaka@gmail.com>
2023-06-08 20:13:09 +0100

    fix gitlabci variable

--- ci-scripts/build_push_images.sh
+++ ci-scripts/build_push_images.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker build applications/${image_name} -t "${T006_REGISTRY_USER_NAME}/t006-${image_name}:${tag_name}"
+docker build applications/${image_name} -t "${T006_REGISTRY_USER_NAME}/t006-${image_name}:${image_tag}"
 
-docker push "${T006REGISTRY_USER_NAME}/t006-${image_name}:${tag_name}"
+docker push "${T006REGISTRY_USER_NAME}/t006-${image_name}:${image_tag}"
