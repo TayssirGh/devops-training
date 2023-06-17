613cc323cb9c4be21e38360daab57fdabfef243f
hhmila <housseinhmila@gmail.com>
2023-06-17 18:58:53 +0100

    build the CI/CD

--- ci-scripts/comp.sh
+++ ci-scripts/comp.sh
@@ -1,3 +1,3 @@
-docker build applications/${image_name} --tag "${T005_REGISTRY_USER_NAME}/t007-${image_name}:${image_tag}"
+docker build applications/${image_name} --tag "${T007_REGISTRY_USER_NAME}/t007-${image_name}:${image_tag}"
 
 
