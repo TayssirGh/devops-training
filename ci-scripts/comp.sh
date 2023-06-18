965a35e28af1a54be900611b0741400cc52b8f9b
hhmila <housseinhmila@gmail.com>
2023-06-18 01:04:25 +0100

    try to fix the CI/CD

--- ci-scripts/comp.sh
+++ ci-scripts/comp.sh
@@ -1,3 +1,6 @@
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
 docker build applications/${image_name} --tag "${T007_REGISTRY_USER_NAME}/t-007-${image_name}:${image_tag}"
 
 
