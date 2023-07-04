cc40208c7b888feb4574b7208d82dbd798e698fa
Houssein Hmila <housseinhmila@gmail.com>
2023-07-04 10:21:05 +0100

    fix the ci/cd

--- comp.sh
+++ comp.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 echo "${T007_REGISTRY_USER_NAME}/t-007-${image_name}:${image_tag}"
+docker tag "${T007_REGISTRY_USER_NAME}/t-007-${image_name}:${image_tag}" "${T007_REGISTRY_USER_NAME}/t-007-${image_name}:${image_tag}"
 docker push "${T007_REGISTRY_USER_NAME}/t-007-${image_name}:${image_tag}"
