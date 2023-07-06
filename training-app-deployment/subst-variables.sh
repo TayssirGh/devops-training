fc9facc44a124f649954a7e3a34c9c206f0c5ea4
xqliza <khoubeibmannai@outlook.com>
2023-07-06 19:41:52 +0100

    test pipeline

--- training-app-deployment/subst-variables.sh
+++ training-app-deployment/subst-variables.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-sed -i "s|#T005_MONGO_URI#|${T005_MONGO_URI}|" k8s-secret-backend.yml
-sed -i "s|#PUBLIC_BUCKET_ENDPOINT#|${PUBLIC_BUCKET_ENDPOINT}|" k8s-secret-qr.yml
-sed -i "s|#PUBLIC_BUCKET_ACCESS_KEY#|${PUBLIC_BUCKET_ACCESS_KEY}|" k8s-secret-qr.yml
-sed -i "s|#PUBLIC_BUCKET_SECRET_KEY#|${PUBLIC_BUCKET_SECRET_KEY}|" k8s-secret-qr.yml
+sed -i "s|#T005_MONGO_URI#|${T005_MONGO_URI}|" training-app-deployment/k8s-secret-backend.yml
+sed -i "s|#PUBLIC_BUCKET_ENDPOINT#|${PUBLIC_BUCKET_ENDPOINT}|" training-app-deployment/k8s-secret-qr.yml
+sed -i "s|#PUBLIC_BUCKET_ACCESS_KEY#|${PUBLIC_BUCKET_ACCESS_KEY}|" training-app-deployment/k8s-secret-qr.yml
+sed -i "s|#PUBLIC_BUCKET_SECRET_KEY#|${PUBLIC_BUCKET_SECRET_KEY}|" training-app-deployment/k8s-secret-qr.yml
