ccc4a300346d874c1b66265b3cd77ae0c95a7e44
Karim Zomita <Zomitaka@gmail.com>
2023-07-11 12:51:34 +0100

    revert piplines

--- training-app-deployment/init-k3d-cluster.sh
+++ training-app-deployment/init-k3d-cluster.sh
@@ -1,6 +1,8 @@
+cd training-app-deployment
+
 k3d cluster delete devopsTraining
 
-k3d cluster create  devopsTraining --config training-app-deployment/k3d-config.yml
+k3d cluster create  devopsTraining --config k3d-config.yml
 
 kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.22.0/controller.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/cloud/deploy.yaml
@@ -16,3 +18,17 @@ kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=-1s
+
+
+bash seal-secrets.sh
+
+# Using Kubctl
+# kubectl apply -k base 
+
+# for file in  sealed-secrets-*.yml ; do
+#     kubectl apply -f $file
+#     rm $file
+# done
+
+# Using Helm
+helm install devops app-chart/
