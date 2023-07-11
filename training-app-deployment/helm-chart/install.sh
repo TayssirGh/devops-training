0cc6dd88b2d866eae2fb73daebe0a55512c6ba86
xqliza <khoubeibmannai@outlook.com>
2023-07-11 12:33:47 +0100

    fix pipeline

--- training-app-deployment/helm-chart/install.sh
+++ training-app-deployment/helm-chart/install.sh
@@ -2,6 +2,23 @@
 
 cd training-app-deployment/helm-chart
 
+k3d cluster delete dev
+
+k3d cluster create --config ../config.yml
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/cloud/deploy.yaml
+
+kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.22.0/controller.yaml
+
+kubectl wait --namespace kube-system \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=-1s
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=-1s
 
 subst_with_sealed(){
 
