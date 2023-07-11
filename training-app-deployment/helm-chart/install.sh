9f59a0e064462186420713b8042df921ce2abdaf
xqliza <khoubeibmannai@outlook.com>
2023-07-11 12:29:59 +0100

    fix pipeline

--- training-app-deployment/helm-chart/install.sh
+++ training-app-deployment/helm-chart/install.sh
@@ -2,30 +2,14 @@
 
 cd training-app-deployment/helm-chart
 
-k3d cluster delete dev
-
-k3d cluster create --config ../config.yml
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/cloud/deploy.yaml
-
-kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.22.0/controller.yaml
-
-kubectl wait --namespace kube-system \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/component=controller \
-  --timeout=-1s
-
-kubectl wait --namespace ingress-nginx \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/component=controller \
-  --timeout=-1s
 
 subst_with_sealed(){
 
   declare -A content
 
-  while IFS="=" read -r key value; do content["$key"]=$value; done < <(
-    yq '.secrets['$1'].values | to_entries | map([.key, .value] | join("=")) | .[]' training-app-deployment/helm-chart/values.yaml
+  while IFS="=" read -r key value;
+  do content["$key"]=$value; done < <(
+    yq '.secrets['$1'].values | to_entries | map([.key, .value] | join("=")) | .[]' values.yaml
   )
 
   for key in "${!content[@]}";
@@ -38,7 +22,7 @@ subst_with_sealed(){
   else
     sealedValue=$(echo -n $value | kubeseal --raw --name $2 --namespace training-app-backend-prod --from-file=/dev/stdin)
   fi
-  yq -i '.secrets['$1'].values.'$key' = "'$sealedValue'"' training-app-deployment/helm-chart/values.yaml
+  yq -i '.secrets['$1'].values.'$key' = "'$sealedValue'"' values.yaml
   done
 
 }
