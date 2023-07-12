d4750e701d3106d72ceaf3a14438d8b8b4f153f8
Oussama_Achour <oachour08@gmail.com>
2023-07-12 11:43:15 +0100

    fixing gitlab-ci

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cd ./applications
 docker-compose build 
+echo "build successful"
 docker push oussachour/qr_python_code:latest
 docker push oussachour/qr_frontend:latest
 docker push oussachour/qr_backend:latest
