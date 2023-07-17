06415d91c5edfbbe1ecb3b31f7e39b25b463055c
youssef <youssefsaidi869@gmail.com>
2023-07-17 11:55:01 +0100

    sealed secrets

--- kubernetes_deployment/sealedSecret.sh
+++ kubernetes_deployment/sealedSecret.sh
@@ -2,8 +2,8 @@ for secret_file in $(ls secret*.yaml); do
   kubeseal < "${secret_file}" > "${secret_file/.yaml/.sealed.yaml}"
 done
 
-rm secret*.yaml
-cd ..
-git add .
-git commit -m "sealed secret"
-git push 
+# rm secret*.yaml
+# cd ..
+# git add .
+# git commit -m "sealed secret"
+# git push 
