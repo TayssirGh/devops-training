682fc4adb973e5ae7b582a8addb207409cc55f38
youssef <youssefsaidi869@gmail.com>
2023-07-17 13:57:09 +0100

    ci cd kubernetes

--- kubernetes_deployment/create_cluster.sh
+++ kubernetes_deployment/create_cluster.sh
@@ -4,6 +4,6 @@
 sudo k3d cluster delete clusster
 
 sudo k3d cluster create --config create_cluster.yaml
-clustername=clusster
-cd ~/.kube
-sudo k3d kubeconfig get ${clustername} > config
+# clustername=clusster
+# cd ~/.kube
+# sudo k3d kubeconfig get ${clustername} > config
