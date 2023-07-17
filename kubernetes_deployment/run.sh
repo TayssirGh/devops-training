a152e02b99572291b59b6fc349115559303e150b
youssef <youssefsaidi869@gmail.com>
2023-07-17 14:28:00 +0100

    ci cd kubernetes

--- kubernetes_deployment/run.sh
+++ kubernetes_deployment/run.sh
@@ -6,10 +6,10 @@ k3d cluster delete clusster
 sudo kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.22.0/controller.yaml
 sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/cloud/deploy.yaml
 
-sudo kubectl wait --namespace kube-system \
- --for=condition=ready pod \
- --selector=app.kubernetes.io/component=controller \
- --timeout=-1s
+# sudo kubectl wait --namespace kube-system \
+#  --for=condition=ready pod \
+#  --selector=app.kubernetes.io/component=controller \
+#  --timeout=-1s
 
 sudo kubectl wait --namespace ingress-nginx \
  --for=condition=ready pod \
