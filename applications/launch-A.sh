d010d4230ca37c82351001e7005752e4de2cb90a
Oussama_Achour <oachour08@gmail.com>
2023-07-18 16:29:55 +0100

    creating the launch.sh files : finishing Ex3-section1

---
+++ applications/launch-A.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+# you should change permissions in order to execute this file . 
+# Type the following command : sudo chmod u+x launch-A.sh
+docker-compose -f docker-compose-A.yml up -d 
---
+++ applications/launch-B.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+# you should change permissions in order to execute this file . 
+# Type the following command : sudo chmod u+x launch-B.sh
+docker-compose -f docker-compose-B.yml up -d 
