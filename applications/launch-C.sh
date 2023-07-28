3ac32fa89fc8778364a7cae84d71e48c384eca9f
Oussama_Achour <oachour08@gmail.com>
2023-07-28 17:04:43 +0100

    changing nginx service , creating launch-C and launch-D.sh

---
+++ applications/launch-C.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+# you should change permissions in order to execute this file . 
+# Type the following command : sudo chmod u+x launch-C.sh
+docker-compose -f docker-compose-C.yml up -d 
---
+++ applications/launch-D.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+# you should change permissions in order to execute this file . 
+# Type the following command : sudo chmod u+x launch-D.sh
+docker-compose -f docker-compose-D.yml up -d 
