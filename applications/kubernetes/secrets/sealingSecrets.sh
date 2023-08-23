88e65c40d349bfac9144ecafbb822325897a06fe
Oussama_Achour <oachour08@gmail.com>
2023-08-23 09:59:56 +0100

    updating gitlab-ci

--- applications/kubernetes/secrets/sealingSecrets.sh
+++ applications/kubernetes/secrets/sealingSecrets.sh
@@ -8,7 +8,7 @@ seal_command="kubeseal -f"
 for file_path in "$input_folder"/*.yml; do
     kind=$(grep -o 'kind: *.*' "$file_path" | cut -d ' ' -f 2)
     if [[ "$kind" == "Secret" ]]; then
-        output_file="${output_folder}/sealed_$(basename "$file_path")"
+        output_file="${output_folder}/$(basename "$file_path")"
         $seal_command "$file_path" -o yaml > "$output_file"
         echo "$file_path sealed to $output_file"
     fi
