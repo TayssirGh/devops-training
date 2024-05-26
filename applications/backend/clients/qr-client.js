4875e0626f3da3848e7ccb418d01ca931aedb778
Bra <mohamedhedibra8@gmail.com>
2024-05-26 19:37:47 +0100

    testing

--- applications/backend/clients/qr-client.js
+++ applications/backend/clients/qr-client.js
@@ -4,8 +4,8 @@ const {
 } = process.env;
 
 class QrClient {
-
     generateQr(qr_id, qr_content) {
+        console.log("Posting on QR_URL: ", QR_URL)
         return axios.default.post(QR_URL, {
             id: qr_id,
             content: qr_content
