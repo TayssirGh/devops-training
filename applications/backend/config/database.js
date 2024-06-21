a8e317b37d04b101ff1d3a16f716d0b7ba98b045
ilef <ilef.rjiba@insat.ucar.tn>
2024-06-21 18:36:26 +0100

    new branch name

--- applications/backend/config/database.js
+++ applications/backend/config/database.js
@@ -5,8 +5,10 @@ const {
     MONGO_DB
 } = process.env
 
+
 const MONGODB_URI=`${MONGO_URI}/${MONGO_DB}`
 
+
 module.exports = databaseConnection = () => {
-    return mongoose.connect(MONGODB_URI)
+    return mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 }
