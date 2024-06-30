59bf1b6b176d9bd5cca271733a58d3f418bdee3b
ilef <ilef.rjiba@insat.ucar.tn>
2024-06-30 22:59:39 +0100

    added launch scripts for Exercice 3, Section 1

---
+++ applications/launch-A copy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#launching the docker-compose related to Exercice 3, Section 1.A
+docker compose up -f docker-compose-A.yml 
---
+++ applications/launch-B.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#launching the docker-compose related to Exercice 3, Section 1.B
+docker compose up -f docker-compose-B.yml 
