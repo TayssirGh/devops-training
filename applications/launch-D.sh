1f5f4583c95783d23866bfa5250f5c00f6b95034
tayssir <tayssir.ghanmi@eniso.u-sousse.tn>
2024-07-01 11:47:45 +0100

    #ex3-s3 testing

--- applications/launch-D.sh
+++ applications/launch-D.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+cat <<EOT >> ./applications/qr/.env
+QR_TMP_FOLDER="./generated_qr"
+QR_FILL_COLOR="#121128"
+QR_BACKGROUND_COLOR="#dcdbdb"
+USE_BUCKET=true
+BUCKET_ENDPOINT="play.min.io:9000"
+BUCKET_ACCESS_KEY="Q3AM3UQ867SPQQA43P2F"
+BUCKET_SECRET_KEY="zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
+BUCKET_NAME="test"
+BUCKET_PATH=""
+EOT
 docker-compose -f ./applications/docker-compose-D.yml up
