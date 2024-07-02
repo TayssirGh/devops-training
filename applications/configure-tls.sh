1b1eb1e4e06736479f63e9be4495cafe2e0ed323
tayssir <tayssir.ghanmi@eniso.u-sousse.tn>
2024-07-02 11:36:03 +0100

    #ex3-s3 tls script

--- applications/configure-tls.sh
+++ applications/configure-tls.sh
@@ -3,6 +3,6 @@ if [ "$EUID" -ne 0 ]; then
   echo "Please run as root"
   exit
 fi
-apt-get update
-apt-get install -y openssl
+zypper refresh
+zypper install openssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
