b349b6f6da5edc94045b7fbd59695338da03837a
ilef <ilef.rjiba@insat.ucar.tn>
2024-07-10 10:00:32 +0100

    permission issue fixed

--- applications/launch-D.sh
+++ applications/launch-D.sh
@@ -7,9 +7,19 @@ apt-get install -y openssl
 
 # Generate a new private key and self-signed certificate valid for 365 days
 
+mkdir -p ./nginx/certs
+
 openssl req -x509 -out ./nginx/certs/localhost.crt -keyout ./nginx/certs/localhost.key \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=localhost' -extensions EXT -config <( \
    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+
+if [ -f ./nginx/certs/localhost.crt ] && [ -f ./nginx/certs/localhost.key ]; then
+  echo "SSL certificate and key created successfully."
+else
+  echo "Failed to create SSL certificate and key."
+  exit 1
+fi
+
 
 docker compose up -f docker-compose-D.yml 
