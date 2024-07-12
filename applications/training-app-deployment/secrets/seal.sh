8a9b4666acf067dad30b210051743aa0f5d29342
tayssir <tayssir.ghanmi@eniso.u-sousse.tn>
2024-07-12 11:45:31 +0100

    #ex3-s5 sealedserets

--- applications/training-app-deployment/secrets/seal.sh
+++ applications/training-app-deployment/secrets/seal.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #installation part--
-sudo apt install helm
 helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
 # shellcheck disable=SC2034
