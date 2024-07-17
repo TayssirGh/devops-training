c3d6cdafae318033136884bdfa3722d2a8a68bbe
ilef <ilef.rjiba@insat.ucar.tn>
2024-07-17 14:47:34 +0100

    Exercice 3, Section 5 complete

--- applications/init-k3d-cluster.sh
+++ applications/init-k3d-cluster.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 k3d cluster create comwork --agents 1 --servers 1 -p 80:80@loadbalancer -p 443:443@loadbalancer -v $(pwd)/clusterData:/data
+echo "_______________________________________________________________________________________"
+
+echo "cluster creation passed successfully !!!!!!!!!!!!!!!!!!!!!"
+
+echo "_______________________________________________________________________________________"
 
 cd applications/training-app-deployment
 chmod +x seal-secrets.sh
