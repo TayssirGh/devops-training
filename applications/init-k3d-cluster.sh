70e9db8254c4b1ad44fa6ca50774702de1ae0ed9
ilef <ilef.rjiba@insat.ucar.tn>
2024-07-17 15:09:12 +0100

    Exercice 3, Section 5 complete

--- applications/init-k3d-cluster.sh
+++ applications/init-k3d-cluster.sh
@@ -2,6 +2,6 @@
 
 k3d cluster create comwork --agents 1 --servers 1 -p 80:80@loadbalancer -p 443:443@loadbalancer -v $(pwd)/clusterData:/data
 
-- echo "____________________________________________________________"
-- echo "init-k3d-cluster.sh finished successfully"
-- echo "____________________________________________________________"
+echo "____________________________________________________________"
+echo "init-k3d-cluster.sh finished successfully"
+echo "____________________________________________________________"
