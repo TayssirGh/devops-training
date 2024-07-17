8d23785e94fcde65cd09ad7d6545133f814898ad
ilef <ilef.rjiba@insat.ucar.tn>
2024-07-17 14:38:42 +0100

    Exercice 3, Section 5 complete

--- applications/training-app-deployment/seal-secrets.sh
+++ applications/training-app-deployment/seal-secrets.sh
@@ -1,32 +1,32 @@
-# #Installation of sealedsecrets
+#Installation of sealedsecrets
 
-# helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
+helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 
-# helm install my-release sealed-secrets/sealed-secrets
+helm install my-release sealed-secrets/sealed-secrets
 
-# # installation of Kubeseal
+# installation of Kubeseal
 
-# KUBESEAL_VERSION=$(curl -s https://api.github.com/repos/bitnami-labs/sealed-secrets/tags | jq -r '.[0].name' | cut -c 2-)
+KUBESEAL_VERSION=$(curl -s https://api.github.com/repos/bitnami-labs/sealed-secrets/tags | jq -r '.[0].name' | cut -c 2-)
 
-# if [ -z "$KUBESEAL_VERSION" ]; then
-#     echo "Failed to fetch the latest KUBESEAL_VERSION"
-#     exit 1
-# fi
+if [ -z "$KUBESEAL_VERSION" ]; then
+    echo "Failed to fetch the latest KUBESEAL_VERSION"
+    exit 1
+fi
 
-# curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION}/kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz"
-# tar -xvzf kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz kubeseal
-# sudo install -m 755 kubeseal /usr/local/bin/kubeseal
+curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION}/kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz"
+tar -xvzf kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz kubeseal
+sudo install -m 755 kubeseal /usr/local/bin/kubeseal
 
-# kubeseal --fetch-cert --controller-name my-release-sealed-secrets --controller-namespace default
+kubeseal --fetch-cert --controller-name my-release-sealed-secrets --controller-namespace default
 
-# # sealing the secrets
+# sealing the secrets
 
-# kubeseal --controller-name my-release-sealed-secrets --controller-namespace default --format yaml < secrets.yml > sealed-secret.yaml
+kubeseal --controller-name my-release-sealed-secrets --controller-namespace default --format yaml < secrets.yml > sealed-secret.yaml
 
-# kubectl apply -f sealed-secret.yaml
+kubectl apply -f sealed-secret.yaml
 
 # deleting regular secrets from the Git tree
 
-git rm --cached secrets.yml
-cd ../../
-echo applications/training-app-deployment/secrets.yml >> .gitignore
+# git rm --cached secrets.yml
+# cd ../../
+# echo applications/training-app-deployment/secrets.yml >> .gitignore
