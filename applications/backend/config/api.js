71a1e2dc673c43319af07d9d8e80b99f70fa00ec
aziz meddeb <medazizmeddeb1@gmail.com>
2024-07-18 10:10:37 +0100

    ex3:s2-B and s3

--- applications/backend/config/api.js
+++ applications/backend/config/api.js
@@ -9,12 +9,20 @@ api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({extended: true}));
 api.use(cors());
 
+api.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
+
 const errorsHandler = (error, req, res, next) => {
     console.error(error)
     const defaultError = {message: error, code: 500}
     res.status(error.code || defaultError.code)
         .send(error || defaultError);
 }
+
 
 api.use('/access', AccessRouter);
 
