f043aeec54371b9e9614e9f6c38755641f25efc1
aziz meddeb <medazizmeddeb1@gmail.com>
2024-07-22 09:52:28 +0100

    gitlab ci fixes

--- ci-scripts/build_push_images.sh
+++ ci-scripts/build_push_images.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 build(){
     docker build applications/${image_name} -t "${T015_REGISTRY_USER_NAME}/t015-${image_name}:${image_tag}"
     docker push "${T015_REGISTRY_USER_NAME}/t015-${image_name}:${image_tag}"
