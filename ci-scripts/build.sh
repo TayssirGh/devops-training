44f59047858cc52fb0648dc482a636b976131acf
tsts <126917969+TayssirGh@users.noreply.github.com>
2024-07-26 14:08:12 +0100

    Update build.sh

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -1,15 +1,1 @@
-#!/usr/bin/env bash
-chmod u+x ./ci-scripts/build.sh 
 
-cd ./applications
-echo "troubelshooting:before build"
-docker-compose -f docker-compose.yml build 
-echo "troubelshooting:build successful"
-  
-docker push oussachour/qr_python_code:latest
-
-  
-docker push oussachour/qr_frontend:latest
-
- 
-docker push oussachour/qr_backend:latest
